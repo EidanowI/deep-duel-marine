5dfbfaddd987792a03c964e966c14593d36f68b7
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-04-18 23:00:42 +0300

    Add Texture class

---
+++ Deep Duel Marine/Deep Duel Marine/Graphics/Textures/Texture.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include "d3d11.h"
+
+#include "Image.h"
+#include "../IBindable.h"
+#include "../Renderer/Renderer.h"
+
+
+
+class Texture : public IBindable{
+public:
+	Texture(std::string file_name) noexcept;
+	~Texture() noexcept;
+
+	void Bind() override;
+	void Bind(int bind_index) override;
+
+private:
+	ID3D11Texture2D* m_pTexture;
+	ID3D11ShaderResourceView* m_pShader_resource_view;
+};
