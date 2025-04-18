30695f2ed637c6d1e5f8b7d07f0894d92df427c6
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-04-18 23:00:29 +0300

    Minor changes at Image class

--- Deep Duel Marine/Deep Duel Marine/Graphics/Textures/Image.cpp
+++ Deep Duel Marine/Deep Duel Marine/Graphics/Textures/Image.cpp
@@ -3,8 +3,19 @@
 
 
 Image::Image(const std::string& name) noexcept {
-	lodepng::decode(m_buffer, m_width, m_height, "Textures/" + name);
+	lodepng::decode(m_data, m_width, m_height, "Textures/" + name);
 }
 Image::~Image() {
-	m_buffer.clear();
+	m_data.clear();
+}
+
+int Image::GetWidth() {
+	return m_width;
+}
+int Image::GetHeight() {
+	return m_height;
+}
+
+std::vector<unsigned char>* Image::GetData() {
+	return &m_data;
 }
--- Deep Duel Marine/Deep Duel Marine/Graphics/Textures/Image.h
+++ Deep Duel Marine/Deep Duel Marine/Graphics/Textures/Image.h
@@ -11,8 +11,13 @@ public:
 	Image(const std::string& name) noexcept;
 	~Image();
 
+	int GetWidth();
+	int GetHeight();
+
+	std::vector<unsigned char>* GetData();
+
 private:
-	std::vector<unsigned char> m_buffer{};
+	std::vector<unsigned char> m_data{};
 	unsigned int m_width = 0;
 	unsigned int m_height = 0;
 };
